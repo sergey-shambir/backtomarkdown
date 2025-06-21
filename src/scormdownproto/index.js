edd28db5597af035435aa4690c94756c83a6fc1c
Сергей Шамбир <sshambir@yandex.ru>
2025-06-21 11:28:36 +0300

    Сделал скрипт для конвертации HTML в Markdown (через topdown) с промежуточной очисткой и извлечением метаданных (через readability.js)

--- src/scormdownproto/index.js
+++ src/scormdownproto/index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs/promises')
+const TurndownService = require('turndown')
+const { join } = require('path')
 
 if (process.argv.length != 4) {
     console.log("Usage: node index.js <url> <output-path>")
@@ -7,9 +9,93 @@ if (process.argv.length != 4) {
 }
 
 const url = process.argv[2];
-const outputPath = process.argv[3];
 
+const outputDir = join(__dirname, 'output')
 const readabilityJsScriptUrl = '/js/readability.js';
+
+async function saveOutputFile(name, description, content) {
+    const stat = await fs.stat(outputDir)
+    if (!stat) {
+        await fs.mkdir(outputDir, {
+            recursive: true
+        })
+    }
+
+    const outputPath = join(outputDir, name)
+    await fs.writeFile(outputPath, content)
+    console.log(`${description} saved to ${outputPath}`)
+}
+
+/**
+ * @typedef {Object} ReadabilityOutput
+ * @property {string} title
+ * @property {string} excerpt
+ * @property {string} byline
+ * @property {string} dir
+ * @property {string} siteName
+ * @property {string} lang
+ * @property {string} publishedTime
+ * @property {string} content
+ */
+
+/**
+ * @param {ReadabilityOutput} article 
+ * @returns {string}
+ */
+function buildMarkdownFrontmatter(article) {
+    const lines = []
+    const addLine = (name, value) => {
+        if (value) {
+            lines.push(`${name}: ${JSON.stringify(value)}`)
+        }
+    }
+
+    addLine('title', article.title)
+    addLine('description', article.excerpt)
+    addLine('date', article.publishedTime)
+    addLine('author', article.byline)
+    addLine('language', article.lang)
+
+    if (lines.length === 0) {
+        return ''
+    }
+
+    return '---\n' + lines.join('\n') + '\n---\n\n'
+}
+
+/**
+ * @param {ReadabilityOutput} article 
+ * @returns {string}
+ */
+function articleToMarkdown(article) {
+    const frontmatter = buildMarkdownFrontmatter(article)
+    const turndownService = new TurndownService({
+        headingStyle: 'atx',
+        hr: '---',
+        bulletListMarker: '-',
+        codeBlockStyle: 'fenced'
+    })
+    const markdown = turndownService.turndown(article.content)
+
+    return frontmatter + markdown
+}
+
+/**
+ * @param {ReadabilityOutput} article 
+ * @returns {string}
+ */
+function readabilityMetadataToJson(article) {
+    const meta = {
+        title: article.title,
+        excerpt: article.excerpt,
+        byline: article.byline,
+        dir: article.dir,
+        siteName: article.siteName,
+        lang: article.lang,
+        publishedTime: article.publishedTime,
+    }
+    return JSON.stringify(meta, null, 2)
+}
 
 (async () => {
     try {
@@ -31,20 +117,12 @@ const readabilityJsScriptUrl = '/js/readability.js';
                 throw new Error('Failed to evaluate readability script')
             }
 
-            const meta = {
-                title: article.title,
-                excerpt: article.excerpt,
-                byline: article.byline,
-                dir: article.dir,
-                siteName: article.siteName,
-                lang: article.lang,
-                publishedTime: article.publishedTime,
-            }
+            const articleMetadataJson = readabilityMetadataToJson(article)
+            const markdown = articleToMarkdown(article)
 
-            await fs.writeFile(outputPath + '.json', JSON.stringify(meta, null, 2))
-            console.log(`Article metadata saved to ${outputPath}.json`)
-            await fs.writeFile(outputPath, article.content)
-            console.log(`Clean readable HTML saved to ${outputPath}`)
+            await saveOutputFile('article.json', 'Metadata', articleMetadataJson)
+            await saveOutputFile('article.html', 'Clean readable HTML', article.content)
+            await saveOutputFile('article.md', 'Markdown', markdown)
         } finally {
             await browser.close()
         }
@@ -54,5 +132,6 @@ const readabilityJsScriptUrl = '/js/readability.js';
         console.error(error.stack)
         process.exit(1);
     }
+    console.log('Completed OK')
 })();
 
